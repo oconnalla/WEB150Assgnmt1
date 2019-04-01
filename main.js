9f8225800f28a5a59341f670b45029c71713168a
oconnalla <oconnalla@gmail.com>
2019-04-01 15:46:44 -0700

    no function needed

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 let sumNum1 = 2;
 let sumNum2 = 4;
+let totalSum = sumNum1 + sumNum2;
 
-function sumVars (sumNum1, sumNum2){
-    totalSum = sumNum1 + sumNum2;
-    return totalSum
-}
-sumVars();
-
-document.write('The sum of ' sumNum1 'and ' sumNum2 'equals' sumVars);
+document.write('The sum of ' + sumNum1 + ' and '  +sumNum2   + ' equals ' + totalSum);
