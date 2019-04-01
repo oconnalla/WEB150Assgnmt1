98e29190207039dc4fa4b10a6147923c0aa3b84f
oconnalla <oconnalla@gmail.com>
2019-04-01 14:37:41 -0700

    practice using document.write method, will need to fix later

---
+++ main.js
@@ -0,0 +1,10 @@
+let sumNum1 = 2;
+let sumNum2 = 4;
+
+function sumVars (sumNum1, sumNum2){
+    totalSum = sumNum1 + sumNum2;
+    return totalSum
+}
+sumVars();
+
+document.write('The sum of ' sumNum1 'and ' sumNum2 'equals' sumVars);
